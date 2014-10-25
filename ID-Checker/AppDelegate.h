1639db9dd22ea8f48d8ef0248ea575f1b259e45a
Chen Hsin Hsuan <aircon.chen.apple@gmail.com>
2014-10-25 13:31:08 +0800

    Initial Commit

---
+++ ID-Checker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ID-Checker
+//
+//  Created by Chen Hsin Hsuan on 2014/10/25.
+//  Copyright (c) 2014年 aircon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ID-Checker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ID-Checker
+//
+//  Created by Chen Hsin Hsuan on 2014/10/25.
+//  Copyright (c) 2014年 aircon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
